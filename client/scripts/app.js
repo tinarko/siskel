3f23ce1a6859bf414d4647f5de56cde554614d14
tinarko <tinarchan@gmail.com>
2017-02-20 16:48:17 -0800

    Complete siskel

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,12 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    if (this.get('like')) {
+      this.set('like', false);
+    } else {
+      this.set('like', true);
+    }
+    return this.get('like');
   }
 
 });
@@ -15,13 +20,14 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change:like', this.sort);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -58,7 +64,10 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    // console.log(this.AppView.render);
+    // console.log(JSON.stringify(this.render));
+    // console.log(this.model);
+    this.model.on('change', this.render, this);
   },
 
   events: {
@@ -66,7 +75,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +88,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
